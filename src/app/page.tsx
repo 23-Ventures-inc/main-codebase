cda2b617e45f0ff5f5122ea546f36c221ef23686
kunal <natwarlaluzumaki@gmail.com>
2025-01-19 15:22:40 +0530

    fix issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
+"use client";
 import FaqSection from "@/components/FaqSection";
 import HomePage from "../components/HomePage";
 import HeroSection from "@/components/HeroSection";
 import OffersBoxs from "@/components/OffersBoxs";
 import HomeEnd from "@/components/HomeEnd";
 import GridBoxes from "@/components/GridBoxes";
+import { motion } from "motion/react";
 
 export default function Home() {
   return (
     <>
-      <div className="relative w-full min-h-screen flex justify-center items-center gap-4 p-8 flex-col overflow-hidden">
+      <motion.div className="relative w-full min-h-screen flex justify-center items-center gap-4 p-8 flex-col overflow-hidden">
         {/* Video background */}
         <video
           src="/home.mp4"
@@ -21,7 +23,7 @@ export default function Home() {
 
         {/* Content */}
         <HomePage />
-      </div>
+      </motion.div>
 
       <HeroSection />
       <OffersBoxs />
